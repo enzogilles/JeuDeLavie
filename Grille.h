4ca6e3838e8fbd7a824cf5e8a2c3a93a7b8b1621
enzogilles <enzo.gilles@viacesi.fr>
2024-11-26 10:58:45 +0100

    Essai

--- Grille.h
+++ Grille.h
@@ -1,1 +1,20 @@
-dggdgdgdgd
+#ifndef GRILLE_H
+#define GRILLE_H
+
+#include <vector>
+#include "Cellule.h"
+
+class Grille {
+    std::vector<std::vector<Cellule>> grille;
+    int largeur, hauteur;
+
+public:
+    void initialiserDepuisFichier(const std::string& fichier);
+    void simulerConsole();
+    int getLargeur() const { return largeur; }
+    int getHauteur() const { return hauteur; }
+    const Cellule& getCellule(int x, int y) const;
+    void mettreAJour();
+};
+
+#endif
